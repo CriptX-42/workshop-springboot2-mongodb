4f10319df8b879b0013a5404a833c31296021c5a
Ricardo Carvalho <ricardo-svc@live.com>
2025-02-23 15:46:41 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/criptx/cursomc/resources/UserResource.java
+++ src/main/java/com/criptx/cursomc/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.criptx.cursomc.resources;
 
+import com.criptx.cursomc.domain.Post;
 import com.criptx.cursomc.domain.User;
 
 import java.net.URI;
@@ -56,5 +57,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
