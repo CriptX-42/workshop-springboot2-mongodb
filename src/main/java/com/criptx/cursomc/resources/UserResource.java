3af5f5eb5bea1bfd1eeca8a117829afbf967be4b
Ricardo Carvalho <ricardo-svc@live.com>
2025-02-22 23:31:42 -0300

    Usando padrao DTO para retornar usuarios

--- src/main/java/com/criptx/cursomc/resources/UserResource.java
+++ src/main/java/com/criptx/cursomc/resources/UserResource.java
@@ -4,8 +4,10 @@ import com.criptx.cursomc.domain.User;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.criptx.cursomc.services.UserService;
+import dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,8 +23,9 @@ public class UserResource {
     private UserService service;
 
     @RequestMapping(method=RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
