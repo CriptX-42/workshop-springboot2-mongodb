eb1c4996ffda2c850107e407cf8986f84401d77b
Ricardo Carvalho <ricardo-svc@live.com>
2025-02-23 15:44:05 -0300

    Referenciando os posts do usuario

--- src/main/java/com/criptx/cursomc/domain/User.java
+++ src/main/java/com/criptx/cursomc/domain/User.java
@@ -1,9 +1,12 @@
 package com.criptx.cursomc.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Document(collection="user")
 public class User implements Serializable {
@@ -14,6 +17,9 @@ public class User implements Serializable {
     private String name;
     private String email;
 
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
+
     public User() {
     }
 
@@ -22,6 +28,14 @@ public class User implements Serializable {
         this.id = id;
         this.name = name;
         this.email = email;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 
     public String getId() {
