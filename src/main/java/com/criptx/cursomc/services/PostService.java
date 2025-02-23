560a3aa4f0f02e83b41325133ebc4b966f0b3734
CriptX-42 <ricardo-svc@live.com>
2025-02-23 17:12:39 -0300

    Consulta simples com query methods

---
+++ src/main/java/com/criptx/cursomc/services/PostService.java
@@ -0,0 +1,27 @@
+package com.criptx.cursomc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.criptx.cursomc.domain.Post;
+import com.criptx.cursomc.repository.PostRepository;
+import com.criptx.cursomc.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public List<Post> findByTitle(String text) {
+        return repo.findByTitleContainingIgnoreCase(text);
+    }
+}
